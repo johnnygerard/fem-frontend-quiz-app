cc467bf9935a098698784ff6ff70539c6806eb4e
Johnny Gérard <john@jgerard.dev>
2025-02-11 23:03:21 +0100

    refactor: use Tailwind CSS default border radius values

--- src/components/app-button.tsx
+++ src/components/app-button.tsx
@@ -8,7 +8,7 @@ const AppButton = (props: ButtonProps) => {
       {...props}
       className={clsx(
         "tb:text-[1.75rem] text-[1.125rem] leading-none font-medium text-white",
-        "bg-purple tb:rounded-lg tb:h-23 tb:p-8 h-14 w-full rounded-sm p-3",
+        "bg-purple tb:rounded-3xl tb:h-23 tb:p-8 h-14 w-full rounded-xl p-3",
         "shadow hover:bg-(image:--white-overlay)",
       )}
     />
