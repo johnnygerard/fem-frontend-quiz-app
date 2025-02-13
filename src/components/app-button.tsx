3b3676007b739efe306b441746d5a849ff2d7e8c
Johnny Gérard <john@jgerard.dev>
2025-02-13 14:22:20 +0100

    feat: apply AppFocusRing to app-button.tsx

--- src/components/app-button.tsx
+++ src/components/app-button.tsx
@@ -1,7 +1,7 @@
+import AppFocusRing from "@/components/app-focus-ring";
 import Text from "@/components/text";
 import { cn } from "@/utils/cn";
 import { memo } from "react";
-import { FocusRing } from "react-aria";
 import { Button, ButtonProps } from "react-aria-components";
 
 type Props = {
@@ -10,22 +10,18 @@ type Props = {
 
 const AppButton = ({ children, className, ...props }: Props) => {
   return (
-    <FocusRing
-      focusRingClass={cn(
-        "inset-ring-2 inset-ring-black tb:inset-ring-4 dark:inset-ring-white",
-      )}
-    >
+    <AppFocusRing>
       <Button
         {...props}
         className={cn(
           "h-14 w-full rounded-xl bg-purple p-3 tb:h-23 tb:rounded-3xl tb:p-8",
-          "shadow outline-none hover:bg-(image:--white-overlay) dark:shadow-dark",
+          "shadow hover:bg-(image:--white-overlay) dark:shadow-dark",
           className,
         )}
       >
         <Text className="text-white">{children}</Text>
       </Button>
-    </FocusRing>
+    </AppFocusRing>
   );
 };
 
