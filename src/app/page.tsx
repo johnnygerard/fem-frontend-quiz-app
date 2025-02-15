27a56e188ab03eb02bf8d0022735b48c17588f5c
Johnny Gérard <john@jgerard.dev>
2025-02-15 22:02:02 +0100

    feat: add quiz-list.tsx to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-"use client";
+import QuizList from "@/components/quiz-list";
+import { readQuizMetadata } from "@/utils/read-quiz-metadata";
 import { memo } from "react";
 
-const Page = () => {
-  return null;
+const Page = async () => {
+  const metadata = await readQuizMetadata();
+
+  return <QuizList list={metadata} />;
 };
 
 export default memo(Page);
---
+++ src/components/quiz-list.tsx
@@ -0,0 +1,31 @@
+import AppLink from "@/components/app-link";
+import QuizDisplay from "@/components/quiz-display";
+import { QuizMetadata } from "@/types/quiz-metadata";
+import { cn } from "@/utils/cn";
+import { memo } from "react";
+
+type Props = Readonly<{
+  list: QuizMetadata[];
+}>;
+
+const QuizList = ({ list }: Props) => {
+  return (
+    <ul className="flex flex-col gap-3 tb:gap-6">
+      {list.map(({ slug, title }) => (
+        <li key={slug}>
+          <AppLink
+            className={cn(
+              "flex items-center gap-4 bg-white p-3 tb:gap-8 dt:p-5",
+              "rounded-xl tb:rounded-3xl",
+            )}
+            href={`/quiz/${slug}`}
+          >
+            <QuizDisplay quizMetadata={{ slug, title }} />
+          </AppLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default memo(QuizList);
