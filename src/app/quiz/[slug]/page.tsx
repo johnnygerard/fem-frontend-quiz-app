f1df0dd5e9412eaefc70c9582fb30ce95a69048b
Johnny Gérard <john@jgerard.dev>
2025-02-17 18:26:37 +0100

    fix: incorrect handling of slug mismatch

--- src/app/quiz/[slug]/page.tsx
+++ src/app/quiz/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Quiz from "@/components/quiz";
 import { QuizData } from "@/types/quiz-data";
 import { readQuizMetadataList } from "@/utils/read-quiz-metadata-list";
 import { shuffle } from "@/utils/shuffle";
+import { notFound } from "next/navigation";
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 import { cwd } from "node:process";
@@ -24,7 +25,7 @@ const Page = async ({ params }: Props) => {
     (metadata) => metadata.slug === slug,
   );
 
-  if (metadata === undefined) throw new Error("Quiz not found");
+  if (metadata === undefined) notFound();
 
   const path = join(cwd(), `data/quiz/${slug}.json`);
   const json = await readFile(path, "utf8");
